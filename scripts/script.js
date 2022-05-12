dad5895f764bfe39460ebbc038c7f47312ccff5a
jlorenzo58 <jlorenzo58@icloud.com>
2022-05-12 18:35:28 -0400

    some changes

--- scripts/script.js
+++ scripts/script.js
@@ -20,35 +20,35 @@ function changeColor(n){
     if(n==1){
         var temp=document.getElementById("box").style.background;
         if(temp==='red'){
-            document.getElementById("box").style.background = "pink";
+            document.getElementById("box").style.background = 'rgb(' + 248 + ',' + 151 + ',' + 151 + ')';
         }
         else{
             document.getElementById("box").style.background="red";
-            document.getElementById("box2").style.background = 'rgb(' + 240 + ',' + 240 + ',' + 113 + ')';
-            document.getElementById("box3").style.background = "lime";
+            document.getElementById("box2").style.background = 'rgb(' + 241 + ',' + 241 + ',' + 183 + ')';
+            document.getElementById("box3").style.background = 'rgb(' + 151 + ',' + 194 + ',' + 151 + ')';
         }
     }
     else if(n==2){
         var temp=document.getElementById("box2").style.background;
         if(temp==='yellow'){
-            document.getElementById("box2").style.background = 'rgb(' + 240 + ',' + 240 + ',' + 113 + ')';
+            document.getElementById("box2").style.background = 'rgb(' + 241 + ',' + 241 + ',' + 183 + ')';
         }
         else{
             document.getElementById("box2").style.background="yellow";
-            document.getElementById("box").style.background = "pink";
-            document.getElementById("box3").style.background = "lime";
+            document.getElementById("box").style.background = 'rgb(' + 248 + ',' + 151 + ',' + 151 + ')';
+            document.getElementById("box3").style.background = 'rgb(' + 151 + ',' + 194 + ',' + 151 + ')';
         }
     }
 
     else if(n==3){
         var temp=document.getElementById("box3").style.background;
         if(temp==="green"){
-            document.getElementById("box3").style.background = "lime";
+            document.getElementById("box3").style.background = 'rgb(' + 151 + ',' + 194 + ',' + 151 + ')';
         }
         else{
             document.getElementById("box3").style.background="green";
-            document.getElementById("box").style.background = "pink";
-            document.getElementById("box2").style.background = 'rgb(' + 240 + ',' + 240 + ',' + 113 + ')';
+            document.getElementById("box").style.background = 'rgb(' + 248 + ',' + 151 + ',' + 151 + ')';
+            document.getElementById("box2").style.background = 'rgb(' + 241 + ',' + 241 + ',' + 183 + ')';
         }
     }
     else{
